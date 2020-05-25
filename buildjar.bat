4c03059a83608aa5f884427a94d92f901874e2f7
EQDiet <EQDiet@protonmail.com>
2020-05-25 17:36:28 +0200

    Update buildjar.bat

--- buildjar.bat
+++ buildjar.bat
@@ -3,6 +3,6 @@ cd target\classes
 echo Manifest-Version: 1.0>META-INF\MANIFEST.MF
 echo Class-Path: .>>META-INF\MANIFEST.MF
 echo Main-Class: EQDietBeta08>>META-INF\MANIFEST.MF
-if not exist ..\..\Release do (mkdir ..\..\Release)
+if not exist ..\..\Release (mkdir ..\..\Release)
 jar cvmf META-INF\MANIFEST.MF ..\..\Release\EQDietBeta0.8.jar *.class
 exit /B

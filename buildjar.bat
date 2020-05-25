b98a2f3ce0e5809bd2d5029e54a280d817bb8867
EQDiet <EQDiet@protonmail.com>
2020-05-26 00:06:03 +0200

    Changed the way buildjar.bat moves through dirs.

--- buildjar.bat
+++ buildjar.bat
@@ -1,9 +1,10 @@
 @echo off
-cd target\classes
+pushd target\classes
 if not exist META-INF (mkdir META-INF)
 echo Manifest-Version: 1.0>META-INF\MANIFEST.MF
 echo Class-Path: .>>META-INF\MANIFEST.MF
 echo Main-Class: EQDietBeta08>>META-INF\MANIFEST.MF
 if not exist ..\..\Release (mkdir ..\..\Release)
 jar cvmf META-INF\MANIFEST.MF ..\..\Release\EQDietBeta0.8.jar *.class
+popd
 exit /B
